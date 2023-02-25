8d322b4904302d3c32e123aac98c1604c08fe4b3
Ivan-Bertaci <ivanbertaci@yahoo.com.br>
2023-02-25 18:46:13 -0300

    Initial commit

---
+++ src/main/java/com/example/springbootandjavarestapi/SpringBootAndJavaRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootandjavarestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndJavaRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndJavaRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootandjavarestapi/SpringBootAndJavaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootandjavarestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndJavaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
